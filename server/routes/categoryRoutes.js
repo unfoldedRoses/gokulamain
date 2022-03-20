bf25b751423987bebaefc291dee410fcfb74da09
NABIN ACHAR <nabinacharofficial@gmail.com>
2022-03-20 21:08:46 +0530

    api

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
     cb(null, Date.now() + "_" + file.originalname.replace(/\s/g, "_"));
