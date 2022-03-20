8cef3bf92531f9163fdb2006c25c8638d8a28011
NABIN ACHAR <nabinacharofficial@gmail.com>
2022-03-20 20:34:54 +0530

    api

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,12 +1,9 @@
 //create a axios instance
 import axios from "axios";
 
-export const baseURL =
-  "http://localhost:5000";
 const token = localStorage.getItem("userInfo");
 
 const axiosInstance = axios.create({
-  baseURL,
   headers: {
     Authorization: `Bearer ${token !== null ? JSON.parse(token).token : ""} `,
   },
