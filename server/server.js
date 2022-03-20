2fca9462b5d219773e6ddd5cd67f03fc1beac4eb
NABIN ACHAR <nabinacharofficial@gmail.com>
2022-03-20 20:27:16 +0530

    api

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.use(notFound)
 // custom error handler
 app.use(errorHandler)
 
-const PORT = 5000
+const PORT = process.env.PORT||5000
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
