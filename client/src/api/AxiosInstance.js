759e1096c22679d88c74f355a8f9ce489300fc5b
NABIN ACHAR <nabinacharofficial@gmail.com>
2022-03-20 20:41:32 +0530

    api

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,9 +1,11 @@
 //create a axios instance
 import axios from "axios";
 
+export const baseURL = "https://gokulaorganicsmart.herokuapp.com";
 const token = localStorage.getItem("userInfo");
 
 const axiosInstance = axios.create({
+  baseURL,
   headers: {
     Authorization: `Bearer ${token !== null ? JSON.parse(token).token : ""} `,
   },
